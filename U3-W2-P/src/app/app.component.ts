2c93772ddf212d1c8e7326a4782f73523753d699
GabrieleColombo <133026314+GabrieleColomb@users.noreply.github.com>
2023-07-31 10:57:15 +0200

    Add files via upload

---
+++ U3-W2-P/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { TodosService } from './todos.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'U3-W2-P';
+  public isTodosPage?:boolean
+  public inputText:string = ""
+  constructor(public srvTodos:TodosService)
+  {}
+
+  cambioPagina():void
+  {
+    this.isTodosPage = ! this.isTodosPage
+  }
+}
